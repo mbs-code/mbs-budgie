5035ae04cf4d994203e4615f0f6f6fe4ce782818
mbs-code <mbscodec@gmail.com>
2021-10-20 21:42:09 +0900

    add: vuepress-plugin-sidebar を導入

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -32,32 +32,6 @@ module.exports = {
     docsDir: '',
     editLinkText: '',
     lastUpdated: false,
-    nav: [
-      {
-        text: 'Guide',
-        link: '/guide/',
-      },
-      {
-        text: 'Config',
-        link: '/config/'
-      },
-      {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
-      }
-    ],
-    sidebar: {
-      '/guide/': [
-        {
-          title: 'Guide',
-          collapsable: false,
-          children: [
-            '',
-            'using-vue',
-          ]
-        }
-      ],
-    }
   },
 
   /**
@@ -66,5 +40,6 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    ['sidebar']
   ]
 }
